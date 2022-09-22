327788a5ca129006972a3bf4b31ec24098860726
MaletyPerun <malety34@gmail.com>
2022-09-22 16:51:54 +0300

    small better

--- src/Main.java
+++ src/Main.java
@@ -17,12 +17,30 @@ class Main {
         System.out.println("path = " + path);
         List<String> lines = readFile(path);
         int firstCount = Integer.parseInt(lines.get(0));
+        int secondCount = Integer.parseInt(lines.get(firstCount + 1));
+        System.out.println(firstCount + " " + secondCount);
         // TODO: 20.09.2022 валидация на корректронсть чисел и их местонахождение
         List<String> firstList = lines.subList(1, firstCount + 1);
         List<String> secondList = lines.subList(firstCount + 2, lines.size());
         List<String> output = prepareToWrite(lines, firstList, secondList);
-        System.out.println(output);
+        List<String> perOutput = finalPrepareToWrite(output, secondList, firstCount, secondCount);
+
+        System.out.println(perOutput);
         write(output, fileName);
+    }
+
+    private static List<String> finalPrepareToWrite(List<String> output, List<String> secondList, int firstCount, int secondCount) {
+        if (firstCount == secondCount) {
+            List<String> filteredList = output.stream()
+                    .filter(str -> str.contains("?") && secondList.stream().noneMatch(str::contains)).collect(Collectors.toList());
+
+            System.out.println(filteredList.size());
+            System.out.println(filteredList);
+
+            List<String> specList = output.stream()
+                    .peek(str -> str.contains(secondList))
+        }
+        return null;
     }
 
     private static void write(List<String> output, String fileName) throws IOException {
@@ -41,8 +59,20 @@ class Main {
         return first.stream()
 //                .map((String s1) -> {s1.concat(":"),
 //                                        s1.concat(second.stream().filter(s2 -> s2.contains(s1)).findAny().toString());})
-                .map(s -> s.concat(":").concat(second.stream().filter(s2 -> s2.contains(s)).findAny().orElse("?")))
+                .map(s -> s.concat(":").concat(second.stream().filter(s2 -> check(s, s2)).findAny().orElse("?")))
                 .collect(Collectors.toList());
+    }
+
+    private static boolean check(String s, String s2) {
+        System.out.println(s2);
+        return Arrays.stream(s.split(" "))
+//                .map(s1 -> Arrays.stream(s2.split(" "))
+                .anyMatch(s1 -> {
+                    System.out.println("*****");
+//                    System.out.println(s1);
+//                    return s1.contains(Arrays.stream(s2.split(" ")).peek(s3 -> System.out.println("s1 = " + s1 + ", s3 = " + s3)).forEach(););
+                    return Arrays.stream(s2.split(" ")).anyMatch(s3 -> s1.contains(s3) || s3.contains(s1));
+                });
     }
 
     private static List<String> readFile(Path path) {
