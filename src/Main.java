025aa6e64718d1e36373c6e4dca9fc58249b577c
MaletyPerun <malety34@gmail.com>
2022-11-15 17:33:23 +0300

    работа как в примерах

--- src/Main.java
+++ src/Main.java
@@ -9,76 +9,108 @@ import java.util.stream.Stream;
 
 class Main {
     public static void main(String[] args) throws IOException {
+
         // TODO: 20.09.2022 разобраться как запускать программу с аргументами и через консоль
 //        args[0] = "C://input2.txt";
 //        String fileName = args[0];
-        String fileName = "C:\\Users\\Алексей\\Desktop\\input.txt";
+        // прочитали имя файла
+        String fileName = "C:\\Users\\Алексей\\Desktop\\input3.txt";
+        // создали объект по имени файла
         Path path = readPath(fileName);
         System.out.println("path = " + path);
+        // прочитал в список линии из файла
         List<String> lines = readFile(path);
+        // определил счетчики слов одного и второго списка
+        if (lines == null)
+            throw new RuntimeException();
         int firstCount = Integer.parseInt(lines.get(0));
         int secondCount = Integer.parseInt(lines.get(firstCount + 1));
         System.out.println(firstCount + " " + secondCount);
-        // TODO: 20.09.2022 валидация на корректронсть чисел и их местонахождение
+        // выделил первый лист
         List<String> firstList = lines.subList(1, firstCount + 1);
+        // выделил второй лист
         List<String> secondList = lines.subList(firstCount + 2, lines.size());
-        List<String> output = prepareToWrite(lines, firstList, secondList);
+        // подготовка к записи в новый файл с бизнес-логикой
+        // первая итерация сопоставления
+        List<String> output = prepareToWrite(firstList, secondList, firstCount, secondCount);
+        // финальная итерация сопоставления (недостающих)
         List<String> perOutput = finalPrepareToWrite(output, secondList, firstCount, secondCount);
 
         System.out.println(perOutput);
-        write(output, fileName);
+        // записть в новый файл необходимый результат
+        write(perOutput, fileName);
     }
 
     private static List<String> finalPrepareToWrite(List<String> output, List<String> secondList, int firstCount, int secondCount) {
-        if (firstCount == secondCount) {
-            List<String> filteredList = output.stream()
-                    .filter(str -> str.contains("?") && secondList.stream().noneMatch(str::contains)).collect(Collectors.toList());
 
-            System.out.println(filteredList.size());
-            System.out.println(filteredList);
+        if (firstCount != secondCount)
+            return output;
 
-            List<String> specList = output.stream()
-                    .peek(str -> str.contains(secondList))
+        // список из output со словами, которые не нашли себе пару
+        List<String> listNotFound = output.stream()
+                .filter(s -> s.contains("?"))
+                .toList();
+
+        // список из output со словами, которые нашли себе пару
+        List<String> listFound = output.stream()
+                .filter(s -> !s.contains("?"))
+                .toList();
+
+        // список слов из второго списка, которые не нашли себе пару с первым списком 
+        List<String> listNotFoundFromSecond = secondList.stream()
+                .filter(s1 -> output.stream()
+                        .noneMatch(s2 -> s2.contains(s1) || s1.contains(s2)))
+                .toList();
+
+        return addWithoutMatches(listFound, listNotFound, listNotFoundFromSecond);
+    }
+
+    private static List<String> addWithoutMatches(List<String> listFound, List<String> listNotFound, List<String> listNotFoundFromSecond) {
+        List<String> addMatches = new ArrayList<>(listFound);
+        System.out.println("addMatches near cicle = " + addMatches);
+        for (int i = 0; i < listNotFound.size(); i++) {
+            addMatches.add(listNotFound.get(i).replace("?", listNotFoundFromSecond.get(i)));
         }
-        return null;
+
+        return addMatches;
     }
 
     private static void write(List<String> output, String fileName) throws IOException {
         String outputFileName = fileName.replace("in", "out");
-//        String outputFileName = "C:\\Users\\Алексей\\Desktop\\output2.txt";
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFileName));
-//        bufferedWriter.newLine();
         Files.write(Paths.get(outputFileName), output, StandardOpenOption.CREATE);
     }
 
-    private static List<String> prepareToWrite(List<String> lines, List<String> first, List<String> second) {
-//        return first.stream()
-//                .filter(s -> second.contains(s))
-//                .collect(Collectors.toList());
+    private static List<String> prepareToWrite(List<String> first, List<String> second, int firstCount, int secondCount) {
 
-        return first.stream()
-//                .map((String s1) -> {s1.concat(":"),
-//                                        s1.concat(second.stream().filter(s2 -> s2.contains(s1)).findAny().toString());})
-                .map(s -> s.concat(":").concat(second.stream().filter(s2 -> check(s, s2)).findAny().orElse("?")))
-                .collect(Collectors.toList());
+        List<String> output = new ArrayList<>(first.stream()
+                .map(s -> s.concat(":").concat(second.stream()
+                        .filter(s2 -> check(s, s2))
+                        .findAny()
+                        .orElse("?")))
+                .toList());
+
+        if (output.size() < Math.max(firstCount, secondCount)) {
+            List<String> addOutput = prepareToWrite(second, first, firstCount, secondCount);
+
+            List<String> addSecOutput = addOutput.stream()
+                    .filter(s -> s.contains("?"))
+                    .toList();
+
+            output.addAll(addSecOutput);
+        }
+
+        return output;
     }
 
-    private static boolean check(String s, String s2) {
-        System.out.println(s2);
-        return Arrays.stream(s.split(" "))
-//                .map(s1 -> Arrays.stream(s2.split(" "))
-                .anyMatch(s1 -> {
-                    System.out.println("*****");
-//                    System.out.println(s1);
-//                    return s1.contains(Arrays.stream(s2.split(" ")).peek(s3 -> System.out.println("s1 = " + s1 + ", s3 = " + s3)).forEach(););
-                    return Arrays.stream(s2.split(" ")).anyMatch(s3 -> s1.contains(s3) || s3.contains(s1));
-                });
+    private static boolean check(String str1, String str2) {
+        return Arrays.stream(str1.split(" "))
+                .anyMatch(s1 -> Arrays.stream(str2.split(" ")).anyMatch(s2 -> s1.contains(s2) || s2.contains(s1)));
     }
 
     private static List<String> readFile(Path path) {
         try (Stream<String> lineStream = Files.lines(path)) {
             return lineStream.collect(Collectors.toList());
-        } catch (Exception e) {
+        } catch (Exception ignored) {
         }
         return null;
     }
